b9002f5e03010273a9d6f2fe6d4e1af7f6c9e25e
Lucas Martins <lucaslm2010@gmail.com>
2020-07-25 00:35:45 -0300

    Alterações

--- back/routes/admin.js
+++ back/routes/admin.js
@@ -9,19 +9,7 @@ const connection = require("../database/connect")
 
 const router = express.Router();
 
-var multer  = require('multer');
-var storage = multer.diskStorage({
-  destination: function (req, file, cb) {
-    cb(null, '../front/public/uploads')
-  },
-  filename: function (req, file, cb) {
-    //const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9)
-    var ext = file.originalname.substr(file.originalname.lastIndexOf('.') + 1)
-    cb(null, file.fieldname + '-' + Date.now() + '.' + ext)
-  }
-})
-
-var upload = multer({ storage: storage })
+const upload = require("../services/upload")
 
 /* ======= Definindo rotas via GET e POST =======*/
 
---
+++ back/services/upload.js
@@ -0,0 +1,15 @@
+var multer  = require('multer');
+var storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, '../front/public/uploads')
+  },
+  filename: function (req, file, cb) {
+    //const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9)
+    var ext = file.originalname.substr(file.originalname.lastIndexOf('.') + 1)
+    cb(null, file.fieldname + '-' + Date.now() + '.' + ext)
+  }
+})
+
+var upload = multer({ storage: storage })
+
+module.exports = upload
